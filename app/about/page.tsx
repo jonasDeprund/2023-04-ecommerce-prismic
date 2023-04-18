64471f5701bf0aa4826329a0c1a0467d2f207e7f
Jonas Deprund <jonasdeprund@gmail.com>
2023-04-18 21:42:42 +0200

    Chapter page route done

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function About() {
+  return (
+    <main>
+      <Link href={'/'}>Navigate to home page</Link>
+      <h1>Welcome to the about page</h1>
+    </main>
+  );
+}
