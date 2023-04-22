44a3e728e70701c20789a85b8d2d7797ec787a0d
Jonas Deprund <jonasdeprund@gmail.com>
2023-04-22 16:57:12 +0200

    client server

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
+'use client';
+
 import Link from 'next/link';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+      const data = await res.json();
+      // setData(todos);
+    };
+    fetchTodos();
+  }, []);
   return (
     <main>
       <Link href={'/about'}>Navigate to home page</Link>
       <h1>Hello Next 13</h1>
+      {data.map((todo) => (
+        <h2 key={todo.id}>{todo.title}</h2>
+      ))}
     </main>
   );
 }
