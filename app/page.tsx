55b95ab6e975eea3f15c2ba107770524a1c8bfe4
Jonas Deprund <jonasdeprund@gmail.com>
2023-04-22 17:01:09 +0200

    server side rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,20 @@
-'use client';
-
 import Link from 'next/link';
-import { useState, useEffect } from 'react';
 
-export default function Home() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    const fetchTodos = async () => {
-      const res = await fetch('https://jsonplaceholder.typicode.com/todos');
-      const data = await res.json();
-      // setData(todos);
-    };
-    fetchTodos();
-  }, []);
+const fetchTodos = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+  const data = await res.json();
+  return data;
+};
+
+export default async function Home() {
+  const todos = await fetchTodos();
+
   return (
     <main>
       <Link href={'/about'}>Navigate to home page</Link>
       <h1>Hello Next 13</h1>
-      {data.map((todo) => (
-        <h2 key={todo.id}>{todo.title}</h2>
+      {todos.map((todo) => (
+        <p key={todo.id}>{todo.title}</p>
       ))}
     </main>
   );
