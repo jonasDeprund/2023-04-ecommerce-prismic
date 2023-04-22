8b0fa484ec4fd43f591bad618fdc25ac3cafa1b8
Jonas Deprund <jonasdeprund@gmail.com>
2023-04-22 18:15:58 +0200

    setting up

---
+++ app/api/todos/route.tsx
@@ -0,0 +1,6 @@
+export async function GET() {
+  // Connecting to my db
+  // Look up the todos of the users
+
+  return Response.json({ message: 'Hello World' });
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,7 @@
-import Link from 'next/link';
-
-const fetchTodos = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/todos');
-  const data = await res.json();
-  return data;
-};
-
 export default async function Home() {
-  const todos = await fetchTodos();
-
   return (
     <main>
-      <Link href={'/about'}>Navigate to home page</Link>
-      <h1>Hello Next 13</h1>
-      {todos.map((todo) => (
-        <p key={todo.id}>{todo.title}</p>
-      ))}
+      <h1>Hello</h1>
     </main>
   );
 }
