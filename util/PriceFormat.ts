17281aa9743b04d6b70bbe1d3803c3204c562b42
Jonas Deprund <jonasdeprund@gmail.com>
2023-06-08 18:04:42 +0200

    add price convert

---
+++ util/PriceFormat.ts
@@ -0,0 +1,8 @@
+const formatPrice = (amount: number) => {
+  return new Intl.NumberFormat('en-us', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(amount / 100);
+};
+
+export default formatPrice;
