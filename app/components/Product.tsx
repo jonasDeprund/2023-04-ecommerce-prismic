b39a49ae6bceb39e56b43925824e859e130833da
Jonas Deprund <jonasdeprund@gmail.com>
2023-06-12 17:31:35 +0200

    price convert module added

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
 import formatPrice from '@/util/PriceFormat';
+import { ProductType } from '@/types/ProductsType';
 
-export default function Product({ name, image, price }) {
+export default function Product({ name, image, price }: ProductType) {
   return (
     <div>
       <Image src={image} alt={name} width={400} height={400} />
       <h1>{name}</h1>
-      {formatPrice(price)}
+      {formatPrice(price as number)}
     </div>
   );
 }
