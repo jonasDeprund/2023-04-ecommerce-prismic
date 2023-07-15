a1dfe394c1f45d0afab4ffa9fd94db8ff5e356c9
Jonas Deprund <jonasdeprund@gmail.com>
2023-07-15 16:16:59 +0200

    add price and get home

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -11,14 +11,18 @@ export default async function Product({ searchParams }: SearchParamTypes) {
         width={600}
         height={600}
       />
-      <div>
-        <h1>{searchParams.name}</h1>
-        <p>{searchParams.description}</p>
-      </div>
-      <div className="flex gap-2">
-        <p className="font-bold text-teal-700">
-          {searchParams.unit_amount && formatPrice(searchParams.unit_amount)}
-        </p>
+      <div className="font-medium text-gray-700">
+        <h1 className="text-2xl py-2">{searchParams.name}</h1>
+        <p className="py-2">{searchParams.description}</p>
+        <p className="py-2">{searchParams.features}</p>
+        <div className="flex gap-2">
+          <p className="font-bold text-teal-700">
+            {searchParams.unit_amount && formatPrice(searchParams.unit_amount)}
+          </p>
+        </div>
+        <button className="my-12 text-white py-2 px-6 font-medium rounded-md bg-teal-700">
+          Add to cart
+        </button>
       </div>
     </div>
   );
