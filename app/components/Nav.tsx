de1435feec2abea87df69d6facea5ca26ad07566
Jonas Deprund <jonasdeprund@gmail.com>
2023-07-17 17:55:22 +0200

    chapter 11 done

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -6,16 +6,23 @@ import Image from 'next/image';
 import Link from 'next/link';
 import Cart from './Cart';
 import { useCartStore } from '@/store';
+import { AiFillShopping } from 'react-icons/ai';
 
 export default function Nav({ user }: Session) {
   const cartStore = useCartStore();
   return (
-    <nav className="flex justify-between items-center py-8">
+    <nav className="flex justify-between items-center py-12">
       <Link href={'/'}>
         <h1>Styled</h1>
       </Link>
       <ul className="flex items-center gap-12">
         {/* {If the user is not logged in} */}
+        <li className="flex items-center text-3xl relative cursor-pointer">
+          <AiFillShopping />
+          <span className="bg-teal-700 text-white text-sm font-bold w-5 h-5 rounded-full absolute left-4 bottom-4 flex items-center justify-center">
+            {cartStore.cart.length}
+          </span>
+        </li>
         {!user && (
           <li className="bg-teal-600 text-white py-2 px-4 rounded-md">
             <button onClick={() => signIn()}>Sign In</button>
