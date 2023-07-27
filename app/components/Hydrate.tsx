c38d73653f70e62f08b445eeb677ca9efdd4df37
Jonas Deprund <jonasdeprund@gmail.com>
2023-07-27 17:26:08 +0200

    hydrated component add

---
+++ app/components/Hydrate.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { ReactNode, useEffect, useState } from 'react';
+
+import React from 'react';
+
+export default function Hydrate({ children }: { children: ReactNode }) {
+  const [isHydrated, setIsHydrated] = useState(false);
+
+  // Wait until Next js is completed
+  useEffect(() => {
+    setIsHydrated(true);
+  }, []);
+  return;
+  <>{isHydrated ? <>children</> : <div>Loading...</div>}</>;
+}
