5e8283afb022d3fa00959239df4fad6794c3db77
Jonas Deprund <jonasdeprund@gmail.com>
2023-08-11 18:02:29 +0200

    add cart functionnality

--- app/product/[id]/AddCart.tsx
+++ app/product/[id]/AddCart.tsx
@@ -2,11 +2,24 @@
 import React from 'react';
 import { useCartStore } from '@/store';
 import { AddCartType } from '@/types/AddCartType';
+import { createStore } from 'zustand';
 
-export default function AddCart({ id, name, unit_amount, quantity, image }) {
+export default function AddCart({
+  id,
+  name,
+  unit_amount,
+  quantity,
+  image,
+}: AddCartType) {
+  const cartStore = useCartStore();
   return (
     <>
-      <button onClick={() => store} className="my-12 text-white py-2 px-6 font-medium rounded-md bg-teal-700">
+      <button
+        onClick={() =>
+          cartStore.addProduct({ id, name, image, unit_amount, quantity })
+        }
+        className="my-12 text-white py-2 px-6 font-medium rounded-md bg-teal-700"
+      >
         Add to cart
       </button>
     </>
--- store.ts
+++ store.ts
@@ -14,7 +14,7 @@ type CartState = {
   isOpen: boolean;
   cart: cartItem[];
   toggleCart: () => void;
-  addProduct: (item: CartItem) => void
+  addProduct: (item: CartItem) => void;
 };
 
 export const useCartStore = create<CartState>()(
@@ -23,17 +23,23 @@ export const useCartStore = create<CartState>()(
       cart: [],
       isOpen: false,
       toggleCart: () => set((state) => ({ isOpen: !state.isOpen })),
-      addProduct: (item) => set((state) =>{
-        const existingItem = state.cart.find(cartItem => cartItem === item.id)
-        if(existingItem){
-          const updatedCart = state.cart.map((cartItem) => {
-            if(cartItem.id === item.id){
-              return {...cartItem, quantity: cartItem.quantity +1}
-            }
-          })
-        }
-      
-      }),
+      addProduct: (item) =>
+        set((state) => {
+          const existingItem = state.cart.find(
+            (cartItem) => cartItem.id === item.id
+          );
+          if (existingItem) {
+            const updatedCart = state.cart.map((cartItem) => {
+              if (cartItem.id === item.id) {
+                return { ...cartItem, quantity: cartItem.quantity + 1 };
+              }
+              return cartItem;
+            });
+            return { cart: updatedCart };
+          } else {
+            return { cart: [...state.cart, { ...item, quantity: 1 }] };
+          }
+        }),
     }),
     { name: 'cart-store' }
   )
