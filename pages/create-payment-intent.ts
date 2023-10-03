b10df4b1412c9ea59c5bd04aa4dbe4322bb48525
Jonas Deprund <jonasdeprund@gmail.com>
2023-10-03 18:34:18 +0200

    add next package

--- pages/create-payment-intent.ts
+++ pages/create-payment-intent.ts
@@ -1,1 +1,10 @@
 import Stripe from 'stripe';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: '2022-11-15',
+});
+
+export default async function handler(req, res) {
+    if(req.method === 'POST')
+}
