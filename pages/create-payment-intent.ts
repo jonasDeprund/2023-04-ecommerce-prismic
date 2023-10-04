28d6bac47909fb679988310fd29fe6454e425b79
Jonas Deprund <jonasdeprund@gmail.com>
2023-10-04 17:49:14 +0200

    res wip

--- pages/create-payment-intent.ts
+++ pages/create-payment-intent.ts
@@ -1,10 +1,22 @@
 import Stripe from 'stripe';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: '2022-11-15',
 });
 
-export default async function handler(req, res) {
-    if(req.method === 'POST')
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // Get the user
+  const userSession = await getServerSession(req, res, authOptions);
+  if (!userSession?.user) {
+    res.status(403).json({ message: 'Not logged in' });
+    return;
+  }
+  // Extract data from the body
+  const { items, payment_intent_id } = req.body;
 }
