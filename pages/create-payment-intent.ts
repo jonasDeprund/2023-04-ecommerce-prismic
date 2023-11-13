47d4343508d7b59c934bb382a1d098b52591b0c8
Jonas Deprund <jonasdeprund@gmail.com>
2023-11-13 19:15:37 +0100

    payment intent wip

--- pages/create-payment-intent.ts
+++ pages/create-payment-intent.ts
@@ -2,10 +2,14 @@ import Stripe from 'stripe';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getServerSession } from 'next-auth';
 import { authOptions } from './api/auth/[...nextauth]';
+import { AddCartType } from '@/types/AddCartType';
+import { PrismaClient } from '@prisma/client';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: '2022-11-15',
 });
+
+const prisma = new PrismaClient()
 
 const calculateOrderAmount = (items= AddCartType[]) =>{
   const totalPrice = items.reduce((acc, item)=>{
@@ -39,11 +43,28 @@ export default async function handler(
         name: item.name,
         description: item.description,
         unit_amount: item.unit_amount,
+        image: item.image,
         quantity: item.quantity
       })),
     }
   };
 
-  res.status(200).json({userSession})
+  // Check if the payment intent exists
+  if(payment_intent_id) {
+    const current_intent = await stripe.paymentIntents.retrieve(payment_intent_id);
+  }else{
+    // Create new order with prisma
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: calculateOrderAmount(items),
+      currency: 'usd',
+      automatic_payment_methods: {enabled: true},
+    })
+    orderData.paymentIntentID = paymentIntent.id;
+    const newOrder = await prisma.order.create({
+      data: orderData,
+    }) 
+  }
+
+  res.status(200).json({message: 'done'})
   return
 }
