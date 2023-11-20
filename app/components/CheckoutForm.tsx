9f7502e9819b6ca3bd606593a1e13f477895289e
Jonas Deprund <jonasdeprund@gmail.com>
2023-11-20 18:03:57 +0100

    strope form wip

--- app/components/CheckoutForm.tsx
+++ app/components/CheckoutForm.tsx
@@ -1,7 +1,15 @@
 'use client';
 import React from 'react';
 import { useState, useEffect } from 'react';
+import { PaymentElement } from '@stripe/react-stripe-js';
+import formatPrice from '@/util/PriceFormat';
 
 export default function CheckoutForm() {
-  return <div>CheckoutForm</div>;
+  return (
+    <form id="payment-form">
+      <PaymentElement id="payment-element" options={{ layout: 'tabs' }} />
+      <h1>Total :</h1>
+      <button id="submit" disabled={isLoading || !stripe || !elements}></button>
+    </form>
+  );
 }
