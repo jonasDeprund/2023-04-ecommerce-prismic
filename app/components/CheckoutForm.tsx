c90272bf5c1ae8effcd6aeab031997e7e3bc532e
Jonas Deprund <jonasdeprund@gmail.com>
2023-12-04 17:51:40 +0100

    stripe info

--- app/components/CheckoutForm.tsx
+++ app/components/CheckoutForm.tsx
@@ -8,6 +8,7 @@ import {
 } from '@stripe/react-stripe-js';
 import formatPrice from '@/util/PriceFormat';
 import { useCartStore } from '@/store';
+import { spawn } from 'child_process';
 
 export default function CheckoutForm({
   clientSecret,
@@ -16,20 +17,51 @@ export default function CheckoutForm({
 }) {
   const stripe = useStripe();
   const elements = useElements();
-
   const [isLoading, setIsLoading] = useState(false);
 
   const cartStore = useCartStore();
 
   const totalPrice = cartStore.cart.reduce((acc, item) => {
-    return acc + item.unit_amount * item.quantity!;
+    return acc + item.unit_amount! * item.quantity!;
   }, 0);
 
+  const formattedPrice = formatPrice(totalPrice);
+
+  useEffect(() => {
+    if (!stripe || !clientSecret) {
+      return;
+    }
+  }, [stripe, clientSecret]);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+    setIsLoading(true);
+
+    stripe
+      .confirmPayment({
+        elements,
+        redirect: 'if_required',
+      })
+      .then((result) => {
+        if (!result.error) {
+          cartStore.setCheckout('success');
+        }
+        setIsLoading(false);
+      });
+  };
+
   return (
-    <form id="payment-form">
+    <form onSubmit={handleSubmit} id="payment-form">
       <PaymentElement id="payment-element" options={{ layout: 'tabs' }} />
-      <h1>Total :</h1>
-      <button id="submit" disabled={isLoading || !stripe || !elements}></button>
+      <h1>Total : {formattedPrice}</h1>
+      <button id="submit" disabled={isLoading || !stripe || !elements}>
+        <span id="button-text">
+          {isLoading ? <span>Loading</span> : <span>Pay now</span>}
+        </span>
+      </button>
     </form>
   );
 }
