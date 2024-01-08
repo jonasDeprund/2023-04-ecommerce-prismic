fbb26bba9ac056d8d51437558e326b1d245408ba
Jonas Deprund <jonasdeprund@gmail.com>
2024-01-08 17:34:57 +0100

    dashboard wip

--- app/components/OrderConfirmed.tsx
+++ app/components/OrderConfirmed.tsx
@@ -3,6 +3,7 @@
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 import dance from '@/public/dance.gif';
+import Link from 'next/link';
 
 export default function OrderConfirmed() {
   return (
@@ -15,6 +16,12 @@ export default function OrderConfirmed() {
         <h1 className="text-xl font-medium">Your order as been placed</h1>
         <h2 className="font-sm my-4">Check your email for the receipt</h2>
         <Image src={dance} className="py-8" alt="dancing man" />
+      </div>
+      <div>
+        <Link href={'/dashbaord'}>
+          <button className="font-medium">Check your order</button>
+        </Link>
+        <button></button>
       </div>
     </motion.div>
   );
