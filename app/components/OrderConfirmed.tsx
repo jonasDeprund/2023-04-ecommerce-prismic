b84aa858879c48673c29de5c6da233eec517bc57
Jonas Deprund <jonasdeprund@gmail.com>
2024-01-09 17:58:27 +0100

    success page wip

--- app/components/OrderConfirmed.tsx
+++ app/components/OrderConfirmed.tsx
@@ -4,8 +4,11 @@ import { motion } from 'framer-motion';
 import Image from 'next/image';
 import dance from '@/public/dance.gif';
 import Link from 'next/link';
+import { useCartStore } from '@/store';
 
 export default function OrderConfirmed() {
+  const cartStore = useCartStore();
+
   return (
     <motion.div
       className="flex items-center justify-center my-12"
@@ -16,12 +19,18 @@ export default function OrderConfirmed() {
         <h1 className="text-xl font-medium">Your order as been placed</h1>
         <h2 className="font-sm my-4">Check your email for the receipt</h2>
         <Image src={dance} className="py-8" alt="dancing man" />
-      </div>
-      <div>
-        <Link href={'/dashbaord'}>
-          <button className="font-medium">Check your order</button>
-        </Link>
-        <button></button>
+        <div className="flex items-center justify-center gap-12">
+          <Link href={'/dashbaord'}>
+            <button className="font-medium">Check your order</button>
+          </Link>
+          <button
+            onClick={() => {
+              cartStore.setCheckout('cart'), cartStore.toggleCart();
+            }}
+          >
+            Create a new order
+          </button>
+        </div>
       </div>
     </motion.div>
   );
