26f319761189c3fda420f70dfc0f7f19b3cf9880
Jonas Deprund <jonasdeprund@gmail.com>
2024-01-29 17:43:53 +0100

    wip webhooks

--- pages/api/webkooks.ts
+++ pages/api/webkooks.ts
@@ -3,6 +3,12 @@ import { PrismaClient } from '@prisma/client';
 import { buffer } from 'micro';
 import { NextApiRequest, NextApiResponse } from 'next';
 
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: '2022-11-15',
 });
@@ -15,4 +21,21 @@ export default async function handler(
 ) {
   const buf = await buffer(req);
   const sig = req.headers('stripe-signature');
+
+  if (!sig) {
+    return res.status(404).send('Missing stripe signature');
+  }
+
+  let event: Stripe.Event;
+  try {
+    event = stripe.webhooks.constructEvent(
+      buf,
+      sig,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+  } catch (err) {
+    return res.status(400).send('webhook error' + err);
+  }
 }
+
+// Handle differents types of event
