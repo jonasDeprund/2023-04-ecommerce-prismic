0e51a55a1fdc5c83248cef1858a8c92227b9bcb0
Jonas Deprund <jonasdeprund@gmail.com>
2024-01-31 17:51:43 +0100

    webhooks wip

--- pages/api/webkooks.ts
+++ pages/api/webkooks.ts
@@ -36,6 +36,25 @@ export default async function handler(
   } catch (err) {
     return res.status(400).send('webhook error' + err);
   }
-}
+  // Handle differents types of event
 
-// Handle differents types of event
+  switch (event.type) {
+    case 'payment_intent.created':
+      const paymentIntent = event.data.object;
+      console.log('payment intent was created');
+      break;
+    case 'charge.succeeded':
+      const charge = event.data.object as Stripe.Charge;
+      if (typeof charge.payment_intent === 'string') {
+        const order = await prisma.order.update({
+          where: { payment_intentID: charge.payment_intent },
+          data: { status: 'complete' },
+        });
+      }
+      break;
+    default: {
+      console.log('Unhandled event type' + event.type);
+    }
+  }
+  res.json({ received: true });
+}
