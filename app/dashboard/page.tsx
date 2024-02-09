866227e29bfe7d1b8f5d9becab7e48c804966231
Jonas Deprund <jonasdeprund@gmail.com>
2024-02-09 17:25:20 +0100

    dashboard wip

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,20 @@
 import React from 'react';
+import { PrismaClient } from '@prisma/client';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import { getServerSession } from 'next-auth';
+
+const fetchOrders = async () => {
+  const prisma = new PrismaClient();
+  const user = await getServerSession(authOptions);
+  if (!user) {
+    return { message: 'Not logged in' };
+  }
+  const order = await prisma.order.findMany({
+    where: { userId: user?.user?.id },
+    include: { products: true },
+  });
+  return orders;
+};
 
 export default function dashbaord() {
   return (
