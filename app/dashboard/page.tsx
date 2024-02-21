9835a64f2cc6110e89065a28f63ea8d28e3db5f0
Jonas Deprund <jonasdeprund@gmail.com>
2024-02-21 17:39:45 +0100

    orders wip

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,7 +7,7 @@ const fetchOrders = async () => {
   const prisma = new PrismaClient();
   const user = await getServerSession(authOptions);
   if (!user) {
-    return { message: 'Not logged in' };
+    return null;
   }
   const orders = await prisma.order.findMany({
     where: { userId: user?.user?.id },
@@ -18,11 +18,24 @@ const fetchOrders = async () => {
 
 export default async function dashbaord() {
   const orders = await fetchOrders();
-  console.log(orders);
-
+  if (orders === null)
+    return <div>You need to be loggued to view your order</div>;
+  if (orders.length === 0)
+    return (
+      <div>
+        <h1>No orders placed</h1>
+      </div>
+    );
   return (
     <div>
-      <h1>Your orders</h1>
+      <div className="font-medium">
+        {orders.map((order) => (
+          <div key={order.id} className="rounded-lg">
+            <h2>Order reference: {order.id}</h2>
+            <p>Time: {new Date(order.createDate)}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
