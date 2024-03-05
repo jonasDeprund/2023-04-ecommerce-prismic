ba779ba60b2442a08757c9d59e5af3c91bb00f7b
Jonas Deprund <jonasdeprund@gmail.com>
2024-03-05 18:00:44 +0100

    chapter finish

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,9 @@ import { PrismaClient } from '@prisma/client';
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 import { getServerSession } from 'next-auth';
 import formatPrice from '@/util/PriceFormat';
+import Image from 'next/image';
+
+export const revalidate = 0;
 
 const fetchOrders = async () => {
   const prisma = new PrismaClient();
@@ -31,7 +34,7 @@ export default async function dashbaord() {
     <div>
       <div className="font-medium">
         {orders.map((order) => (
-          <div key={order.id} className="rounded-lg">
+          <div key={order.id} className="rounded-lg p-8 my-12">
             <h2>Order reference: {order.id}</h2>
             <p>Time: {new Date(order.createDate)}</p>
             <p className="text-md py-2">
@@ -48,7 +51,17 @@ export default async function dashbaord() {
             <div className="flex gap-8"></div>
             {order.products.map((product) => (
               <div className="py-2" key={product.id}>
-                <h2>{product.name}</h2>
+                <h2 className="py-2">{product.name}</h2>
+                <div className="flex items-center gap-4">
+                  <Image
+                    src={product.image!}
+                    width={36}
+                    height={36}
+                    alt={product.name}
+                  />
+                  <p>{formatPrice(product.unit_amount)}</p>
+                  <p>Quantity : {product.quantity}</p>
+                </div>
               </div>
             ))}
           </div>
